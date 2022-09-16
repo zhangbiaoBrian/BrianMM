9cb6cda6eb4811d47da39ba0c62835ffbea087c6
Brian <>
2022-09-16 23:45:18 +0800

    提交

---
+++ BrianMM/Classes/TestA.h
@@ -0,0 +1,17 @@
+//
+//  TestA.h
+//  TestOC
+//  -
+//  Created by Brian on 2022/9/16.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestA : NSObject
+
+-(void)test;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BrianMM/Classes/TestA.m
@@ -0,0 +1,15 @@
+//
+//  TestA.m
+//  TestOC
+//
+//  Created by Brian on 2022/9/16.
+//
+
+#import "TestA.h"
+
+@implementation TestA
+
+-(void)test{
+    NSLog(@"就是一个测试");
+}
+@end
